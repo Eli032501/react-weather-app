0de644aadc90f8d434c6ac0db4380bece5e82e11
Eliana Pereira <98112983+Eli032501@users.noreply.github.com>
2024-05-21 11:00:48 +0100

    added footer

--- src/App.js
+++ src/App.js
@@ -2,5 +2,39 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          <p>
+            This project was coded by{" "}
+            <a
+              href="https://github.com/Eli032501"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Eliana Pereira
+            </a>{" "}
+            and is on{" "}
+            <a
+              href="https://github.com/Eli032501/react-weather-app"
+              target="_blank"
+              rel="noreferrer"
+            >
+              GitHub
+            </a>{" "}
+            and hosted on{" "}
+            <a
+              href="https://react-weather-forecast-eli.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Netlify
+            </a>
+          </p>
+        </footer>
+      </div>
+    </div>
+  );
 }
