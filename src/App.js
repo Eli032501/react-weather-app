679c7d3e6b79789fdd5031d05d1b56d8bdd98591
Eliana Pereira <98112983+Eli032501@users.noreply.github.com>
2024-05-21 10:32:43 +0100

    remove unnecessary content

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
+import React from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
+export default function App() {
+  return <div className="App">Weather App</div>;
 }
-
-export default App;
