423929b744f8aa095fb0c7d5515cca66a3b1cb27
Eliana Pereira <98112983+Eli032501@users.noreply.github.com>
2024-05-27 15:17:45 +0100

    forecast responsive

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -16,14 +16,20 @@ export default function WeatherForecast({ coordinates }) {
     return (
       <div className="WeatherForecast container ">
         <div className="forecast-line row ">
-          <div className="col  ">
+          <div className="col ">
             <WeatherForecastDay forecastData={forecast[1]} />
           </div>
-          <div className="col  ">
+          <div className="col ">
             <WeatherForecastDay forecastData={forecast[2]} />
           </div>
-          <div className="col  ">
+          <div className="col ">
             <WeatherForecastDay forecastData={forecast[3]} />
+          </div>
+          <div className="col d-none d-sm-flex">
+            <WeatherForecastDay forecastData={forecast[4]} />
+          </div>
+          <div className="col d-none d-md-flex">
+            <WeatherForecastDay forecastData={forecast[5]} />
           </div>
         </div>
       </div>
